5dec1732eab2179540f9b3cc3fc412c3b5941abe
Vladimir Elizarov <vladimirelizarov89@gmail.com>
2023-07-04 10:31:07 +0300

    fix dz3

--- lesson3/cmd/gosearch/gosearch.go
+++ lesson3/cmd/gosearch/gosearch.go
@@ -14,43 +14,29 @@ func main() {
 	fmt.Println("start")
 	var sFlag = flag.String("s", "", "filter message for crawler")
 	flag.Parse()
-	var url1 string = "https://go.dev"
-	var url2 string = "https://habr.com/"
+	urls := []string{"https://go.dev", "https://habr.com/"}
 	var depth int8 = 2
 	var total = []crawler.Document{}
 	var currentDocumentNumber int = 0
 
 	s := spider.New()
-	result_godev, err := s.Scan(url1, int(depth))
-	if err != nil {
-		fmt.Println(err)
-	}
-	result_golang, err := s.Scan(url2, int(depth))
-	if err != nil {
-		fmt.Println(err)
-	}
+	for _, url := range urls {
+		scanResult, err := s.Scan(url, int(depth))
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	for _, v := range result_godev {
-		total = append(total,
-			crawler.Document{
-				ID:    currentDocumentNumber,
-				URL:   v.URL,
-				Title: v.Title,
-				Body:  v.Body,
-			},
-		)
-		currentDocumentNumber = currentDocumentNumber + 1
-	}
-	for _, v := range result_golang {
-		total = append(total,
-			crawler.Document{
-				ID:    currentDocumentNumber,
-				URL:   v.URL,
-				Title: v.Title,
-				Body:  v.Body,
-			},
-		)
-		currentDocumentNumber = currentDocumentNumber + 1
+		for _, v := range scanResult {
+			total = append(total,
+				crawler.Document{
+					ID:    currentDocumentNumber,
+					URL:   v.URL,
+					Title: v.Title,
+					Body:  v.Body,
+				},
+			)
+			currentDocumentNumber = currentDocumentNumber + 1
+		}
 	}
 
 	for _, v := range total {
